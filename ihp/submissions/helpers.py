ad1cae30676e6b7868747b5caf12d5c4b2880152
Adi Eyal <adi@burgercom.co.za>
2010-07-13 20:53:10 +0200

    Added parsing
    Also added management command

---
+++ ihp/submissions/helpers.py
@@ -0,0 +1,36 @@
+import xlrd
+from submissions.models import Submission, DPQuestion
+
+def parse_file(filename):
+    book = xlrd.open_workbook(filename)
+    for sheet in book.sheets():
+        if sheet.name == "DPs":
+            parse_dp(sheet)
+        else:
+            print >> sys.stderr, "Unknown sheet: %s" % sheet.name
+
+def unfloat(val):
+    if type(val) == float:
+        return str(int(val))
+    return val
+    
+def parse_dp(sheet):
+    country = sheet.cell(0, 5).value
+    institution = sheet.cell(1, 5).value
+    version = sheet.cell(2, 5).value
+    submission = Submission.objects.create(
+        country=country,
+        institution=institution,
+        docversion=version
+    )
+
+    for row in range(5, sheet.nrows):
+        DPQuestion.objects.create(
+           submission=submission,
+           question_number=unfloat(sheet.cell(row, 3).value),
+           baseline_year=unfloat(sheet.cell(row, 5).value),
+           baseline_value=unfloat(sheet.cell(row, 6).value),
+           latest_year=unfloat(sheet.cell(row, 7).value),
+           latest_value=unfloat(sheet.cell(row, 8).value),
+           comments=sheet.cell(row, 11).value,  
+        )
--- ihp/submissions/models.py
+++ ihp/submissions/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class SubmissionManager(models.Manager):
+    def get_latest_submission(self, country, institution):
+        return self.filter(
+            country=country,
+            institution=institution
+        ).reverse()[0:1] or None
+
+class Submission(models.Model):
+    country = models.CharField(max_length=30, null=False)
+    institution = models.CharField(max_length=30, null=False)
+    docversion = models.CharField(max_length=10, null=False)
+    date_submitted = models.DateTimeField(auto_now_add=True)
+
+    objects = SubmissionManager()
+
+class DPQuestion(models.Model):
+    submission = models.ForeignKey(Submission, null=False)
+    question_number = models.CharField(max_length=10, null=False)
+    baseline_year = models.CharField(max_length=4, null=False)
+    baseline_value = models.CharField(max_length=20, null=False)
+    latest_year = models.CharField(max_length=4, null=False)
+    latest_value = models.CharField(max_length=20, null=False)
+    comments = models.TextField()
