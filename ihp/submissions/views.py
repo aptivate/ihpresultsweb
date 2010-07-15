70abd6325c07367c012c73e0a77a07f749a1ce56
Adi Eyal <adi@burgercom.co.za>
2010-07-16 00:22:42 +0200

    Added key for dabbledb

--- ihp/submissions/views.py
+++ ihp/submissions/views.py
@@ -16,6 +16,7 @@ def agency_scorecard(request, template_name="submissions/agency_scorecard.html",
             for question_number, country, comment in old_comments:
                 comments.append("%s %s] %s" % (question_number, country, comment))
             d["comments"] = "\n".join(comments)
+            d["key"] = "%s_%s" % (agency, indicator)
         
     extra_context["targets"] = targets
 
