acb33fd6b105790804269c40c524d4050962dbc6
Adi Eyal <adi@burgercom.co.za>
2011-01-15 17:49:52 +0200

    Installed django debug toolbar

--- ihp/settings.py
+++ ihp/settings.py
@@ -15,6 +15,7 @@ DATABASE_NAME = 'ihp.db'
 
 TIME_ZONE = 'Africa/Johannesburg'
 LANGUAGE_CODE = 'en-us'
+INTERNAL_IPS = ("127.0.0.1",)
 
 SITE_ID = 1
 
@@ -50,6 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'ihp.urls'
@@ -64,6 +66,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    "debug_toolbar",
    
     'submissions',
 )
@@ -74,3 +77,16 @@ INSTALLED_APPS = (
 POLL_USERNAME = "ihpresults2010survey@human-scale.net"
 POLL_PASSWORD = "changeme"
 POLL_HOST = "pop.gmail.com"
+
+# Debug Toolbar
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
