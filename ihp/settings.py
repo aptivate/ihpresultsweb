177959e579eff9d251a01501a2992317efdc9d18
Adi Eyal <adi@burgercom.co.za>
2011-01-17 09:28:13 +0200

    Disabled django debug toolbar

--- ihp/settings.py
+++ ihp/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+#    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'ihp.urls'
