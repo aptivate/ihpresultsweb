924848899af88a13441b1322986e99f8de709972
Adi Eyal <adi@burgercom.co.za>
2011-01-19 10:25:38 +0200

    Changed spm language

--- ihp/submissions/views.py
+++ ihp/submissions/views.py
@@ -545,7 +545,8 @@ def perc_change(base_val, latest_val):
 
 
 spm_map = {
-    "1DP" : "Proportion of ihp+ countries in which the partner has signed commitment to (or documented support for) the ihp+ country compact, or equivalent agreement.",
+    #"1DP" : "Proportion of ihp+ countries in which the partner has signed commitment to (or documented support for) the ihp+ country compact, or equivalent agreement.",
+    "1DP" : "Partner has signed commitment to (or documented support for) the IHP+ country compact, or equivalent agreement, where they exist.",
     "2DPa" : "Percent of aid flows to the health sector that is reported on national health sector budgets.",
     "2DPb" : "Percent of current capacity-development support provided through coordinated programmes consistent with national plans/strategies for the health sector.",
     "2DPc" : "Percent of health sector aid provided as programme based approaches.",
@@ -554,8 +555,10 @@ spm_map = {
     "5DPa" : "Percent of health sector aid that uses country procurement systems.",
     "5DPb" : "Percent of health sector aid that uses public financial management systems.",
     "5DPc" : "Number of parallel project implementation units (pius) per country.",
-    "6DP" : "Proportion of countries in which agreed, transparent and monitorable performance assessment frameworks are being used to assess progress in the health sector.",
-    "7DP" : "Proportion of countries where mutual assessments have been made of progress implementing commitments in the health sector, including on aid effectiveness.",
+    #"6DP" : "Proportion of countries in which agreed, transparent and monitorable performance assessment frameworks are being used to assess progress in the health sector.",
+    "6DP" : "Partner uses the single national performance assessment framework, where they exist, as the primary basis to assess progress (of support to health sector).",
+    #"7DP" : "Proportion of countries where mutual assessments have been made of progress implementing commitments in the health sector, including on aid effectiveness.",
+    "7DP" : "Partner has participated in mutual assessment of progress implementing commitments in the health sector, including on aid effectiveness, if a mutual assessment process exists.",
     "8DP" : "Evidence of support for civil society to be actively represented in health sector policy processes - including health sector planning, coordination & review mechanisms.",
 }
         
