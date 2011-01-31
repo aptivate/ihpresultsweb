734d31b8c6bb3f7b19273937274886aed24c996c
Chris Wilson <chris-github@aptivate.org>
2011-01-31 16:32:38 +0000

    Start testing that indicator functions work as expected.

--- ihp/publicweb/tests.py
+++ ihp/publicweb/tests.py
@@ -1,23 +1,28 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+Tests for the public IHP website Django parts.
 """
 
 from django.test import TestCase
+from submissions.models import Agency, Country, AgencyCountries
+from submissions.target import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class PublicWebsiteTest(TestCase):
+    fixtures = ['indicator_tests']
+    
+    def test_getting_agency_countries(self):
+        unicef = Agency.objects.get(agency="UNICEF")
+        self.assertNotEqual(unicef, None)
+        self.assertEqual(unicef.id, 27)
+        
+        mozambique = Country.objects.get(country="Mozambique")
+        self.assertNotEqual(mozambique, None)
+        self.assertEqual(mozambique.id, 9)
+        
+        agency_countries = AgencyCountries.objects.get_agency_countries(unicef)
+        self.assertTrue(mozambique in agency_countries)
+        
+        self.assertTrue(country_agency_progress(mozambique, unicef))
+        
+        
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
 
