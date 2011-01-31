d8cfaea05feee64d5c4c645498dc7c8efd480cfd
Adi Eyal <adi@burgercom.co.za>
2011-01-31 16:40:31 +0200

    Added 2nd level criterion for 4Ga

--- ihp/submissions/target.py
+++ ihp/submissions/target.py
@@ -45,6 +45,9 @@ def evaluate_indicator(target, base_val, cur_val):
             if base_val not in [None, NA_STR]:
                 if cur_val - base_val >= 0.5:
                     return Rating.TICK
+        elif target.indicator in ["4Ga"]:
+            if cur_val <= 20:
+                return Rating.TICK
 
     try:
         if tick_func(base_val, cur_val, target.tick_criterion_value):
