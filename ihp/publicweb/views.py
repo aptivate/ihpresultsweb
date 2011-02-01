2667e475140e8a39124ceb36bfee055676329df8
Chris Wilson <chris-github@aptivate.org>
2011-02-01 12:49:09 +0000

    Put the agency object into the request context, instead of just its name.

--- ihp/publicweb/views.py
+++ ihp/publicweb/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from submissions.models import Agency
 
 def agency_scorecard_page(request, agency_name):
+    agency = Agency.objects.get(agency=agency_name)
     return render_to_response('agency_scorecard.html',
-        RequestContext(request, {'agency_name': agency_name}))
+        RequestContext(request, {'agency': agency}))
