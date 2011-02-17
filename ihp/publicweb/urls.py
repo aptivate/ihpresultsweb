caffc9530f7e0bcd638de9d670417b650344bf08
Chris Wilson <chris-github@aptivate.org>
2011-02-17 15:15:24 +0000

    Allow spaces in country names in public scorecard URL mapping.

--- ihp/publicweb/urls.py
+++ ihp/publicweb/urls.py
@@ -6,10 +6,10 @@ from django.views.generic.list_detail import object_list
 from ihp.publicweb.views import *
 
 urlpatterns = patterns('ihp.publicweb.views',
-        url(r'^scorecard/agency/(?P<agency_name>[-\w]+)/$',
+        url(r'^scorecard/agency/(?P<agency_name>[-\w ]+)/$',
             agency_scorecard_page,
             name='public-agency-scorecard'),
-        url(r'^scorecard/country/(?P<country_name>[-\w]+)/$',
+        url(r'^scorecard/country/(?P<country_name>[-\w ]+)/$',
             country_scorecard_page,
             name='public-country-scorecard'),
     )
