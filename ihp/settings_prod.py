c327af3a31df289df2109f2f51700d5be28ab11e
Adi Eyal <adi@burgercom.co.za>
2011-02-18 15:14:31 +0000

    Updated settings to include POLL parameters

--- ihp/settings_prod.py
+++ ihp/settings_prod.py
@@ -2,3 +2,7 @@ from settings import *
 MEDIA_URL = "/redia/"
 ADMIN_MEDIA_PREFIX = '/media/'
 DEBUG = True
+
+POLL_USERNAME = "ihpresults2010survey@human-scale.net"
+POLL_PASSWORD = "changeme"
+POLL_HOST = "pop.gmail.com"
