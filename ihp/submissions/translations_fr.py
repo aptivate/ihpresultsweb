23cf945544c3789dca430a71bb05517f8f75f1e4
Adi Eyal <adi@burgercom.co.za>
2011-02-28 23:57:36 +0200

    Added unicode to french translations

--- ihp/submissions/translations_fr.py
+++ ihp/submissions/translations_fr.py
@@ -49,8 +49,8 @@ gov_commentary_text = {
     },
 }
 
-rating_question_text = "Une quantité insuffisante de données a été fournie pour permettre d’évaluer cette mesure de la performance standard."
-rating_none_text = "Cette mesure de la performance standard a été jugée non applicable au %s"
+rating_question_text = u"Une quantité insuffisante de données a été fournie pour permettre d’évaluer cette mesure de la performance standard."
+rating_none_text = u"Cette mesure de la performance standard a été jugée non applicable au %s"
 
 gov_tb2 = "%s COUNTRY SCORECARD"
 gov_pc3 = "%0.1f %% allocated to health"
