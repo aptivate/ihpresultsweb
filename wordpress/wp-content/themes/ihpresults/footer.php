b11960a77fc15a540da8c1f74ed04a668e7ab942
Chris Wilson <chris-github@aptivate.org>
2011-03-18 16:51:46 +0000

    Add Re-Action logo and copyright to website footer.

--- wordpress/wp-content/themes/ihpresults/footer.php
+++ wordpress/wp-content/themes/ihpresults/footer.php
@@ -5,9 +5,29 @@
 <div id="footer">
 <div id="footer-inner">
 	<div id="footer-left">
-		 <div class="copyright">statement about this website and any rights associated with it</div>
-		 <?php wp_nav_menu( array('theme_location' => 'tertiary' ) ); ?>
-		
+		<div class="copyright">
+			<a href="http://www.re-action.co.za"><img style="width: 100px;"
+				src="/wp-content/uploads/2011/02/logo21.jpg"
+				alt="Re-Action Logo"></a>
+			<a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/"><img
+			 	alt="Creative Commons License" style="border-width:0"
+			 	src="http://i.creativecommons.org/l/by-nc/3.0/88x31.png" /></a>
+			<br />
+			<span xmlns:dct="http://purl.org/dc/terms/"
+			 	href="http://purl.org/dc/dcmitype/Text" property="dct:title"
+			 	rel="dct:type">IHP+Results Performance Monitoring and
+			 	Accountability Mechanism for the International Health
+			 	Partnership	(IHP+)</span> by <a
+			 	xmlns:cc="http://creativecommons.org/ns#"
+				href="http://www.re-action.co.za" property="cc:attributionName"
+				rel="cc:attributionURL">Re-Action! (UK) Ltd</a> is licensed
+				under a	<a rel="license"
+				href="http://creativecommons.org/licenses/by-nc/3.0/">Creative
+				Commons Attribution-NonCommercial 3.0 Unported License</a>.
+			Based on a work at <a xmlns:dct="http://purl.org/dc/terms/"
+				href="http://www.ihpresults.net" rel="dct:source">www.ihpresults.net</a>
+		</div>
+		<?php wp_nav_menu( array('theme_location' => 'tertiary' ) ); ?>
 	</div><!-- end footer-left -->
   	<div id="footer-right">
   		<div id="logins"><?php wp_loginout( $redirect ); ?> | <a href="/wp-admin">admin</a> | <a href="http://www.google.com/analytics">stats</a></div>
