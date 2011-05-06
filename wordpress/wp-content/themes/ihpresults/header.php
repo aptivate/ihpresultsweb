0287599cb1b3da67b553c25f4b731145d8890dae
Chris Wilson <chris-github@aptivate.org>
2011-05-06 14:26:06 +0100

    Remove the analytics code from the wrong website.

--- wordpress/wp-content/themes/ihpresults/header.php
+++ wordpress/wp-content/themes/ihpresults/header.php
@@ -77,19 +77,6 @@
 	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 	wp_head();
 ?>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20361977-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </head>
 <body <?php if(function_exists('body_class')) { body_class(); } ?>>
 
